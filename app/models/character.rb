45cba9e8ea7de4d4447efa95b925dc3c3b29e3e4
Max Montesino <45675504+m-nt-s-no@users.noreply.github.com>
2024-12-14 16:28:53 +0000

    replaced model accessor methods with belongs_to and has_many meta-methods

--- app/models/character.rb
+++ app/models/character.rb
@@ -10,15 +10,6 @@
 #  movie_id   :integer
 #
 class Character < ApplicationRecord
-  belongs_to(:movie, class_name: "Movie", foreign_key: "movie_id")
-
-  #def movie
-    #key = self.movie_id
-
-    #matching_set = Movie.where({ :id => key })
-
-    #the_one = matching_set.at(0)
-
-    #return the_one
-  end
+  belongs_to(:movie)
+  belongs_to(:actor)
 end
